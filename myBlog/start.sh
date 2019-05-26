d599db362747cad74f27860da633caed9b6af38f
WIZeaz <764696642@qq.com>
2019-05-26 22:26:08 +0800

    Update start.sh

--- myBlog/start.sh
+++ myBlog/start.sh
@@ -1,6 +1,7 @@
 pip install django
 pip install markdown
 pip install pillow
+pip install pygments
 python3 manage.py makemigrations
 python3 manage.py migrate
 nohup python3 manage.py runserver 0.0.0.0:80 > server.stdout 2> server.stderr & disown
