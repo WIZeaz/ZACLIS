7f0183e8391aeb68e961d3b9088a6ce065685dca
WIZeaz <764696642@qq.com>
2019-05-26 22:25:45 +0800

    fix some bug

--- myBlog/start.bat
+++ myBlog/start.bat
@@ -1,6 +1,9 @@
+@echo off
 pip install django
 pip install markdown
 pip install pillow
+pip install pygments
 python manage.py makemigrations
 python manage.py migrate
+echo =========server ready for running=========
 python manage.py runserver 0.0.0.0:80
